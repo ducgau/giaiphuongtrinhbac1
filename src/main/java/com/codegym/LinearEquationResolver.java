82cf9aeb9c7495e442a6de2cdb284b8a49832ea8
admin <ducgau99@gmail.com>
2019-03-28 17:32:44 +0800

    finish

---
+++ src/main/java/com/codegym/LinearEquationResolver.java
@@ -0,0 +1,27 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a,b,c;
+        System.out.print("a: ");
+        a = scanner.nextDouble();
+        System.out.print("b: ");
+        b = scanner.nextDouble();
+        System.out.print("c: ");
+        c = scanner.nextDouble();
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        } else {
+            if (b == 0) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+
+    }
+}
